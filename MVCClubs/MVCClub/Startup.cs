f2c207fc2946ca864dd49e2b5ae5ab8873af749a
s00083446 <s00083446@mail.itsligo.ie>
2019-07-20 07:27:13 +0100

    Classes added and migrations enabled

---
+++ MVCClubs/MVCClub/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCClub.Startup))]
+namespace MVCClub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
